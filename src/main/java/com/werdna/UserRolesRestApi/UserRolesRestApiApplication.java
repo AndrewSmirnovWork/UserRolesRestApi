484abdba8f217c94f67d114a72eb41f36690f2f4
AndrewSmirnovWork <andrew.smirnov.work@gmail.com>
2020-11-18 16:37:23 +0300

    first commit

---
+++ src/main/java/com/werdna/UserRolesRestApi/UserRolesRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.werdna.UserRolesRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRolesRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRolesRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/werdna/UserRolesRestApi/UserRolesRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.werdna.UserRolesRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRolesRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
